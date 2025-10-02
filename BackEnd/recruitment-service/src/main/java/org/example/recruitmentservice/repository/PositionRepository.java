4d0dde236634edc348625fb4e09ef1c9c9d994e6
Chi Pham <chi.pham@s3corp.com.vn>
2025-10-02 16:55:36 +0700

    add 2 Api to filter and search position

--- BackEnd/recruitment-service/src/main/java/org/example/recruitmentservice/repository/PositionRepository.java
+++ BackEnd/recruitment-service/src/main/java/org/example/recruitmentservice/repository/PositionRepository.java
@@ -2,9 +2,25 @@ package org.example.recruitmentservice.repository;
 
 import org.example.recruitmentservice.models.Positions;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PositionRepository extends JpaRepository<Positions, Integer> {
     Optional<Positions> findByNameAndLanguageAndLevel(String name, String language, String level);
+    @Query("SELECT p FROM Positions p " +
+            "WHERE (:name IS NULL OR p.name = :name) " +
+            "AND (:language IS NULL OR p.language = :language) " +
+            "AND (:level IS NULL OR p.level = :level)")
+    List<Positions> findByFilters(@Param("name") String name,
+                                  @Param("language") String language,
+                                  @Param("level") String level);
+
+    @Query("SELECT p FROM Positions p " +
+            "WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "   OR LOWER(p.language) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "   OR LOWER(p.level) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Positions> searchByKeyword(@Param("keyword") String keyword);
 }
